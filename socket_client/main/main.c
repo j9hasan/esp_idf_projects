1dd041490991327caf72327aebbaa5ff2649ccbc
jubaid hasan <jubaid.eee07@gmail.com>
2024-05-14 11:03:12 +0600

    Update main.c

--- socket_client/main/main.c
+++ socket_client/main/main.c
@@ -1,11 +1,4 @@
-/* BSD Socket API Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
